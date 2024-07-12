35e991475d6f5a84561e73baea4d5a41cf383519
Aman Upadhyay <146805438+amanupadhyay-obj@users.noreply.github.com>
2024-07-12 16:57:05 +0530

    createBrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
 import RestaurantCard from "./components/RestaurantCard";
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
 
 const Allayout=()=>{
     return(
@@ -13,6 +15,19 @@ const Allayout=()=>{
     );
 };
 
+//Here we make use of createRouterBrowser and its take a list of object and object contain the path and the element
+
+const appRouter=createBrowserRouter([
+  {
+    path:"/",
+    element:<Allayout/>
+  },
+  {
+    path:"/about",
+    element:<About/>
+  }
+])
+
 // Get the root element and render the parent element into it
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Allayout/>);
+root.render(<RouterProvider router={appRouter}/>);
