f1c9ecf06a1bf6f73ad4ad83f79afbe5d8ee197c
Aman Upadhyay <146805438+amanupadhyay-obj@users.noreply.github.com>
2024-07-12 16:56:40 +0530

    createBrowserRouter

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+
+
+const About=()=>{
+    return(
+        <div>
+        <h1>This is About Us Page </h1>
+        <h2>Hey my name is Aman Upadhyay !!</h2>
+        </div>
+    )
+}
+
+export default About;
