30bd821226820202fbab9b93547eb6572a58e23a
Aman Upadhyay <146805438+amanupadhyay-obj@users.noreply.github.com>
2024-07-22 21:32:51 +0530

    tailwindandaddingaddbutton

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,11 +4,12 @@ import { CDN_URL } from "../utils/constants";
 const ItemList=({items})=>{
     console.log(items);
     return(
-      <div>
+      <div >
         {items.map((item)=>(
             <div key={item.card.info.id}
-            className="p-2 m-2 border-gray-200 border-b-2 text-left flex ">
-            <div  className="p-2">
+            className="p-4 m-2 border-gray-200 border-b-2 text-left flex justify-between ">
+             <div className="w-9/12"> 
+            <div  className="py-2">
                 <span>{item.card.info.name}</span>
                 <span>
                 - ₹
@@ -16,7 +17,14 @@ const ItemList=({items})=>{
                   ? item.card.info.price / 100
                   : item.card.info.defaultPrice / 100}
               </span>
-                </div>
+              </div>
+                </div >
+                <div className="w-3/12 p-4">
+                <div className="absolute">
+              <button className="p-1 bg-black text-white shadow-lg mx-4 rounded-lg">ADD +</button>
+              </div>
+              <img src={CDN_URL+item.card.info.imageId} className="w-full"/>
+              </div>
         </div>
     ))}
       </div>
